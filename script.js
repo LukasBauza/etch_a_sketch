b6323bdc56deb3cf7f97667d4bfe6e38bd9e53ec
Lukas Bauza <lukasbauza.lb@proton.me>
2025-06-07 21:38:14 +0100

    Change the the size of the grid

--- script.js
+++ script.js
@@ -1,20 +1,41 @@
 var div = document.querySelector("div");
+var button = document.querySelector("button");
 
-for (let c = 0; c < 16; c++) {
-    var col_clone = div.cloneNode();
-    col_clone.style.flexDirection = "column";
-	for (let r = 0; r < 16; r++) {
-		var row_clone = div.cloneNode();
-		row_clone.classList.add("tile");
-		col_clone.appendChild(row_clone);
+button.addEventListener("click", () => {
+	var grid_size = prompt("Enter the size of the grid");
+
+	if (grid_size <= 100) {
+		console.log("Grid Size: " + grid_size);
+		removeGrid();
+		drawGrid(grid_size);
 	}
-    div.appendChild(col_clone);
+});
+
+drawGrid(16);
+
+function drawGrid(grid_size) {
+	for (let c = 0; c < grid_size; c++) {
+	    var col_clone = div.cloneNode();
+	    col_clone.style.flexDirection = "column";
+		for (let r = 0; r < grid_size; r++) {
+			var row_clone = div.cloneNode();
+			row_clone.classList.add("tile");
+			col_clone.appendChild(row_clone);
+		}
+	    div.appendChild(col_clone);
+	}
+
+	var tiles = document.getElementsByClassName("tile");
+
+	for (var i = 0; i < tiles.length; i++) {
+		tiles[i].addEventListener("mouseover", function(e) {
+			this.style.backgroundColor = "black";
+		});
+	}
 }
 
-var tiles = document.getElementsByClassName("tile");
-
-for (var i = 0; i < tiles.length; i++) {
-	tiles[i].addEventListener("mouseover", function(e) {
-		this.style.backgroundColor = "black";
-	});
+function removeGrid() {
+	while (div.firstChild) {
+		div.removeChild(div.firstChild);
+	}
 }
